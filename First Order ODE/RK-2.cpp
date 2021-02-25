04eb3df003d28300776e40730a58b4c307407a21
Yachu Joshi <yachujoshi1234@gmail.com>
2021-02-25 22:03:12 +0545

    Added RK-2 Method

---
+++ First Order ODE/RK-2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+float function(float x, float y)
+{
+  return 2 * y / x;
+}
+
+int main()
+{
+  float initialX, initialY, finalX, finalY, xValue, yValue, stepSize, n, k, k1, k2;
+  cout << "Enter inital x, y: ";
+  cin >> initialX >> initialY;
+  cout << "Enter step size: ";
+  cin >> stepSize;
+  cout << "Enter value of x for which y is to be determined: ";
+  cin >> finalX;
+
+  n = (finalX - initialX) / stepSize;
+  xValue = initialX;
+  yValue = initialY;
+
+  cout << "X\t"
+       << "Y" << endl;
+
+  for (int i = 0; i < n; i++)
+  {
+    k1 = stepSize * function(xValue, yValue);
+    k2 = stepSize * function(xValue + stepSize, yValue + k1);
+    k = (k1 + k2) / 2;
+    xValue += stepSize;
+    yValue += k;
+    cout << xValue << "\t" << setprecision(5) << yValue << endl;
+  }
+
+  return 0;
+}
